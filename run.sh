1f773c2cdd9fdbf67e6a0f14b516eb377f4424bd
renasya <165255921+renasyafatya@users.noreply.github.com>
2024-03-28 14:39:04 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-27adad0e-bb31-4cfc-90c4-1b7db3a828f6/)
 
 
 echo "$curl_output" >> Update.tsx
